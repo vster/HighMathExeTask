d5535488ae7c190544fc9fd17f88d79afbd4ec29
Vital Teresh <vster2006@gmail.com>
2018-02-07 09:27:26 +0300

    modify ex215

--- part1/1.1.5_Systems_of_liner_equations/ex215.m
+++ part1/1.1.5_Systems_of_liner_equations/ex215.m
@@ -3,3 +3,9 @@ syms x y alpha
                       x*sin(alpha)+y*cos(alpha)==sin(2*alpha))
 sol_x=simplify(sol_x)
 sol_y=simplify(sol_y)
+
+eq1=x*cos(alpha)-y*sin(alpha)-cos(2*alpha)
+eq2=x*sin(alpha)+y*cos(alpha)-sin(2*alpha)
+[sol_x,sol_y] = solve(eq1,eq2)
+sol_x=simplify(sol_x)
+sol_y=simplify(sol_y)
