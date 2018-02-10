e0677d31765b60eaca7df2c47e7f296c0f02b3d2
Vital Teresh <vster2006@gmail.com>
2018-02-10 21:48:13 +0300

    add ex 1329-1331

--- 1.9.1.1_Direct_integration/ex1331.m
+++ 1.9.1.1_Direct_integration/ex1331.m
@@ -1,15 +1,19 @@
 syms x;
-y=2*x^3-5*x^2+7*x-3
+
+y=(1+x^2)^(1/2)*x
+pretty(y)
 subplot(3,1,1)
 ezplot(y)
 grid
 
 F=simplify(int(y))
+pretty(F)
 subplot(3,1,2)
 ezplot(F)
 grid
 
-y2=diff(F)
+y2=simplify(diff(F))
+pretty(y2)
 subplot(3,1,3)
 ezplot(y2)
 grid
