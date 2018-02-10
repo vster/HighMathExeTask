0a82af0d7eea54876e0466865727f2d03c4f1820
Vital Teresh <vster2006@gmail.com>
2018-02-10 12:06:16 +0300

    add ex 798-801

---
+++ 1.7.1.1_Derivative_and_differential/ex799.m
@@ -0,0 +1,21 @@
+syms x
+
+y=log(((x-1)*(x-3)^3)/((x-2)^3*(x-4)))
+pretty(y)
+subplot(3,1,1)
+ezplot(y)
+grid
+
+dy=diff(y)
+dy=simplify(dy)
+pretty(dy)
+
+subplot(3,1,2)
+ezplot(dy)
+grid
+
+y2=simplify(int(dy))
+pretty(y2)
+subplot(3,1,3)
+ezplot(y2)
+grid
