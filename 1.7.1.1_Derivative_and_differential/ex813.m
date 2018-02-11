fa6d3865a26df43422c5e6c701b39192ae0d2f28
Vital Teresh <vster2006@gmail.com>
2018-02-11 15:12:33 +0300

    add ex 810-818

---
+++ 1.7.1.1_Derivative_and_differential/ex813.m
@@ -0,0 +1,21 @@
+syms x
+
+y=asin(exp(x))+asin(sqrt(1-exp(2*x)))
+pretty(y)
+subplot(3,1,1)
+ezplot(y)
+grid
+
+dy=diff(y)
+dy=simplify(dy)
+pretty(dy)
+
+subplot(3,1,2)
+ezplot(dy)
+grid
+
+y2=simplify(int(dy))
+pretty(y2)
+subplot(3,1,3)
+ezplot(y2)
+grid
---
+++ 1.7.1.1_Derivative_and_differential/ex816.m
@@ -0,0 +1,21 @@
+syms x
+
+y=x^2+2*x*sin(x)*cos(x)+cos(x)^2
+pretty(y)
+subplot(3,1,1)
+ezplot(y)
+grid
+
+dy=diff(y)
+dy=simplify(dy)
+pretty(dy)
+
+subplot(3,1,2)
+ezplot(dy)
+grid
+
+y2=simplify(int(dy))
+pretty(y2)
+subplot(3,1,3)
+ezplot(y2)
+grid
