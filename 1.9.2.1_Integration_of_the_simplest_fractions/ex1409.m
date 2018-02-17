b67f2311f4c2088bff7b18db9d0156866608b86d
Vital Teresh <vster2006@gmail.com>
2018-02-18 02:03:04 +0300

    add ex 1403-1418

---
+++ 1.9.2.1_Integration_of_the_simplest_fractions/ex1409.m
@@ -0,0 +1,19 @@
+syms x;
+
+y=(3*x+2)/(x^2+2*x+10)^2
+pretty(y)
+subplot(3,1,1)
+ezplot(y)
+grid
+
+F=simplify(int(y))
+pretty(F)
+subplot(3,1,2)
+ezplot(F)
+grid
+
+y2=simplify(diff(F))
+pretty(y2)
+subplot(3,1,3)
+ezplot(y2)
+grid
