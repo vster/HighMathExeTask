9913d6d70973fd326704592896a94c980cbdf99f
Vital Teresh <vster2006@gmail.com>
2018-02-20 23:57:17 +0300

    add ex1424-1440

--- 1.7.5_The_vector_valued_function_of_a_scalar_argument/ex1141.m
+++ 1.7.5_The_vector_valued_function_of_a_scalar_argument/ex1141.m
@@ -2,8 +2,6 @@ syms t;
 r=[1/sqrt(2)*exp(t)*sin(t);1;1/sqrt(2)*exp(t)*cos(t)]
 t0=0
 
-ezplot3(r(1),r(2),r(3))
-
 dr=simplify(diff(r,t))
 
 rM=subs(r,t,t0)
@@ -13,4 +11,8 @@ e1=(x-rM(1))/drM(1)
 e2=(y-rM(2))/drM(2)
 e3=(z-rM(3))/drM(3)
 
-en=drM(1)*(x-rM(1))+drM(2)*(y-rM(2))+drM(3)*(z-rM(3))
+en=simplify(drM(1)*(x-rM(1))+drM(2)*(y-rM(2))+drM(3)*(z-rM(3)))
+
+ezplot3(r(1),r(2),r(3))
+hold on
+ezmesh(en)
