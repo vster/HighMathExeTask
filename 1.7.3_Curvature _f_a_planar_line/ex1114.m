6e370e7bc74dd465cb4a5d474155e65c5b6b10d3
Vital Teresh <vster2006@gmail.com>
2018-02-22 22:00:54 +0300

    mod ex 1114

--- 1.7.3_Curvature _f_a_planar_line/ex1114.m
+++ 1.7.3_Curvature _f_a_planar_line/ex1114.m
@@ -9,5 +9,17 @@ DFy=diff(F,y)
 Dyx=simplify(-diff(F,x)/diff(F,y))
 pretty(Dyx)
 
+syms g(x);
+Dgx=-9*x/(25*g(x))
+pretty(Dgx)
+
+D2gx=diff(Dgx,x)
+pretty(D2gx)
+
+D2yx=9*x*Dyx/(25*y^2)-9/(25*y)
+pretty(D2yx)
+D2yx=simplify(D2yx)
+pretty(D2yx)
+
 % Не закончено!
 % 2-я производная неявной фукнции
