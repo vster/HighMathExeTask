53d1807f2fa6a14479c63c12b745bc9346e8c174
Vital Teresh <vster2006@gmail.com>
2018-02-22 14:58:26 +0300

    mod ex 1112-1113

--- 1.7.3_Curvature _f_a_planar_line/ex1112.m
+++ 1.7.3_Curvature _f_a_planar_line/ex1112.m
@@ -2,15 +2,28 @@ syms x y;
 F=x^3+y^4-2
 M=[1 1]
 
-ezsurf(F)
+% ezsurf(F)
 
 DFx=diff(F,x)
 DFy=diff(F,y)
 
 Dyx=simplify(-diff(F,x)/diff(F,y))
 pretty(Dyx)
-
 DyxM=subs(Dyx,[x y],M)
+
+syms g(x);
+Dgx=-3*x^2/(4*g(x)^3)
+pretty(Dgx)
+
+D2gx=simplify(diff(Dgx,x))
+pretty(D2gx)
+
+D2yx=-x*(2*y-3*x*Dyx)*3/(4*y^4)
+pretty(D2yx)
+simplify(D2yx)
+pretty(D2yx)
+D2yxM=subs(Dyx,[x y],M)
+
 
 % Не закончено!
 % 2-я производная неявной фукнции
--- 1.7.3_Curvature _f_a_planar_line/ex1113.m
+++ 1.7.3_Curvature _f_a_planar_line/ex1113.m
@@ -1,13 +1,25 @@
 syms x y;
 F=2*y^2-2*x-1
 
-ezsurf(F)
+% ezsurf(F)
 
 DFx=diff(F,x)
 DFy=diff(F,y)
 
 Dyx=simplify(-diff(F,x)/diff(F,y))
 pretty(Dyx)
+
+syms g(x);
+Dgx=1/(2*g(x))
+pretty(Dgx)
+
+D2gx=diff(Dgx,x)
+pretty(D2gx)
+
+D2yx=-Dyx/(2*y^2)
+pretty(D2yx)
+D2yx=simplify(D2yx)
+pretty(D2yx)
 
 % Не закончено!
 % 2-я производная неявной фукнции
