b11aaac502c68bf31d7149c9665c23090232595b
Vital Teresh <vster2006@gmail.com>
2018-02-23 18:46:34 +0300

    add ex 1447-1462

---
+++ 1.9.3_Integration_of_the_simplest_irrational_functions/ex1453.m
@@ -0,0 +1,19 @@
+syms x;
+y=1/(sqrt(1-2*x)-(1-2*x)^(1/4))
+
+pretty(y)
+subplot(3,1,1)
+ezplot(y)
+grid
+
+F=simplify(int(y))
+pretty(F)
+subplot(3,1,2)
+ezplot(F)
+grid
+
+y2=simplify(diff(F))
+pretty(y2)
+subplot(3,1,3)
+ezplot(y2)
+grid
