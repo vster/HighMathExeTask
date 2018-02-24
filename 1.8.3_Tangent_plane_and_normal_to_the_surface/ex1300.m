46b220c56fb483cedae65602bd97be8f10effeed
Vital Teresh <vster2006@gmail.com>
2018-02-24 22:20:59 +0300

    add ex 1299-1300

---
+++ 1.8.3_Tangent_plane_and_normal_to_the_surface/ex1300.m
@@ -0,0 +1,23 @@
+syms x y;
+z=sin(x)*cos(y)
+M=[pi/4 pi/4 1/2]
+
+ezsurf(z)
+hold on
+plot3(M(1),M(2),M(3),'ro')
+
+Dzx=diff(z,x)
+Dzy=diff(z,y)
+
+DzxM=subs(Dzx,[x y z],M)
+DzyM=subs(Dzy,[x y z],M)
+
+zn=M(3)+DzxM*(x-M(1))+DzyM*(y-M(2))
+ezsurf(zn)
+
+syms xn yn zn;
+eq1=(xn-M(1))/DzxM
+eq2=(yn-M(2))/DzyM
+eq3=(zn-M(3))/(-1)
+
+hold off
