266d033abb904bb53fbc086bcd6f717ab5382e3f
Vital Teresh <vster2006@gmail.com>
2018-02-24 12:12:58 +0300

    add ex 1463-1471

---
+++ 1.9.3_Integration_of_the_simplest_irrational_functions/ex1464.m
@@ -0,0 +1,19 @@
+syms x a;
+y=x^3/((a^2-x^2)*sqrt(a^2-x^2))
+
+pretty(y)
+subplot(3,1,1)
+ezplot(y)
+grid
+
+F=simplify(int(y))
+pretty(F)
+subplot(3,1,2)
+ezplot(F)
+grid
+
+y2=simplify(diff(F))
+pretty(y2)
+subplot(3,1,3)
+ezplot(y2)
+grid
