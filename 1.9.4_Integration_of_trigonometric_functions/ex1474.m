9840bedc36f1a5533839cdb76823677494b9f818
Vital Teresh <vster2006@gmail.com>
2018-02-25 12:31:43 +0300

    mod 1472-1481 add  1482-1500

--- 1.9.4_Integration_of_trigonometric_functions/ex1474.m
+++ 1.9.4_Integration_of_trigonometric_functions/ex1474.m
@@ -17,3 +17,5 @@ pretty(y2)
 subplot(3,1,3)
 ezplot(y2)
 grid
+
+c=simplify(y-y2)
--- 1.9.4_Integration_of_trigonometric_functions/ex1487.m
+++ 1.9.4_Integration_of_trigonometric_functions/ex1487.m
@@ -1,21 +1,21 @@
-syms x a b;
-y=1/((a^2+b^2)-(a^2-b^2)*cos(x))
+syms x;
+y=sin(2*x)*cos(5*x)
 
 pretty(y)
 subplot(3,1,1)
-%ezplot(y)
+ezplot(y)
 grid
 
 F=simplify(int(y))
 pretty(F)
 subplot(3,1,2)
-%ezplot(F)
+ezplot(F)
 grid
 
 y2=simplify(diff(F))
 pretty(y2)
 subplot(3,1,3)
-%ezplot(y2)
+ezplot(y2)
 grid
 
 c=simplify(y-y2)
