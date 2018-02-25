f912a75dd94dfd714284f45d8049d8bdfb31b13a
Vital Teresh <vster2006@gmail.com>
2018-02-25 14:01:06 +0300

    add ex 1501-1505

---
+++ 1.9.4_Integration_of_trigonometric_functions/ex1503.m
@@ -0,0 +1,21 @@
+syms x;
+y=sin(3*x)*sin(x)
+
+pretty(y)
+subplot(3,1,1)
+ezplot(y)
+grid
+
+F=simplify(int(y))
+pretty(F)
+subplot(3,1,2)
+ezplot(F)
+grid
+
+y2=simplify(diff(F))
+pretty(y2)
+subplot(3,1,3)
+ezplot(y2)
+grid
+
+c=simplify(y-y2)
