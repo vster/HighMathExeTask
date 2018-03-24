8f9c6c4e7addc5a01169124db09ec2e1a7e4652f
Vital Teresh <vster2006@gmail.com>
2018-03-25 00:56:22 +0300

    modify 1662-1668

--- 1.10.9_Calculation_of_work_and_pressure/ex1665.m
+++ 1.10.9_Calculation_of_work_and_pressure/ex1665.m
@@ -4,7 +4,15 @@ g=9.8
 ro=1000
 r=d/2
 
-dP=2*ro*g*x*sqrt(r^2-x^2)*dx
+dP=2*ro*g*x*sqrt(r^2-x^2)
 % 19600*dx*x*(9 - x^2)^(1/2)
 P=int(19600*x*(9 - x^2)^(1/2),x,0,r)
+% 176400
+
+clear
+syms x r ro g real;
+dP=2*ro*g*x*sqrt(r^2-x^2)
+P=int(dP,x,0,r)
+% P=(2*g*r^3*ro)/3
+P=subs(P,[g r ro],[9.8 3 1000])
 % 176400
--- 1.10.9_Calculation_of_work_and_pressure/ex1668.m
+++ 1.10.9_Calculation_of_work_and_pressure/ex1668.m
@@ -1,21 +1,13 @@
-syms x y dx real;
-d=0.06
-g=9.8
-ro=1000
-r=d/2
-c=1
-
-% syms d g ro r c;
+syms g ro r c x;
 dP=2*ro*g*(c+r+x)*sqrt(r^2-x^2)
-
 P=int(dP,x,-r,r)
-
-d=0.06
-g=9.8
-ro=1000
-r=d/2
-c=1
+% pi*g*r^2*ro*(c + r)
+d=0.06  % м
+g=9.8   % м/с^2
+ro=1000 % кг/м^3
+r=d/2   % м
+c=1     % м
 
 P=pi*g*r^2*ro*(c + r)
+% 28.5401 (?)
 
-% Проверить!
