6ac2b7680cc509992bcb8c556e7913980e8730c8
Vital Teresh <vster2006@gmail.com>
2018-04-07 15:47:22 +0300

    Surface_area_calculation add ex 70-71

---
+++ 2.12.5_Surface_area_calculation/ex70.m
@@ -0,0 +1,15 @@
+syms x z;
+y=x^2+z^2
+% D: x^2+z^2=1
+
+fs=sqrt(1+diff(y,x)^2+diff(y,z)^2)
+% (4*x^2 + 4*z^2 + 1)^(1/2)
+syms ro teta;
+x=ro*cos(teta)
+z=ro*sin(teta)
+fs=(4*x^2 + 4*z^2 + 1)^(1/2)
+fs=simplify(fs)
+% (4*ro^2 + 1)^(1/2)
+
+S=int(int(fs*ro,ro,0,1),teta,0,pi/2)
+% (pi*(5*5^(1/2) - 1))/24
