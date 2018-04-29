31e0c36bd5db8e7dce09892080eb4734f7859320
Vital Teresh <vster2006@gmail.com>
2018-04-29 14:49:02 +0300

    add ex 87-88

---
+++ 2.12.6_Physical_applications_of_the_double_integral/ex87.m
@@ -0,0 +1,15 @@
+% Найти ЦТ
+% y=sqrt(2*x-x^2), y=0
+
+syms x y;
+y1=sqrt(2*x-x^2)
+
+ezplot(y1)
+grid on
+
+S=int(int(1,y,0,sqrt(2*x-x^2)),x,0,2)
+% pi/2
+xm=1/S*int(int(x,y,0,sqrt(2*x-x^2)),x,0,2)
+% 1
+ym=1/S*int(int(y,y,0,sqrt(2*x-x^2)),x,0,2)
+% 4/(3*pi)
---
+++ 2.12.6_Physical_applications_of_the_double_integral/ex88.m
@@ -0,0 +1,25 @@
+% Вычислить момент инерции
+% y=2*sqrt(x), x+y=3, x=0, y=0
+
+syms x y;
+y1=2*sqrt(x)
+y2=3-x
+
+ezplot(y1)
+hold on
+ezplot(y2)
+hold off
+grid on
+
+x1=(y/2)^2
+x2=3-y
+
+slv=solve(y==2*sqrt(x), x+y==3)
+[slv.x slv.y]
+% [ 4 - 2*3^(1/2), 2*3^(1/2) - 2]
+Ix=int(int(y^2,x,x1,x2),y,0,2)
+vpa(Ix)
+% 2.4
+Iy=int(int(x^2,x,x1,x2),y,0,2)
+vpa(Iy)
+% 6.57
