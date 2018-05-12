87d6c8f250d1d7e7cdd5514d527522bc7222d478
Vital Teresh <vster2006@gmail.com>
2018-05-12 19:27:40 +0300

    add ex 200

--- 2.13.1_Curvilinear_integrals_along_the_length_of_the_arc_and_along_the_coordinates/ex200.m
+++ 2.13.1_Curvilinear_integrals_along_the_length_of_the_arc_and_along_the_coordinates/ex200.m
@@ -2,9 +2,17 @@
 % где OA - четверть окружности
 % x=cos(t),y=sin(t),z=1
 
-syms t
+syms t dt;
 x=cos(t)
 y=sin(t)
-z=1
-f=x*y*diff(x)+y*z*diff(y)+z*x*diff(z)
+z=sym(1)
+dx=diff(x)*dt
+dy=diff(y)*dt
+dz=diff(z)*dt
+
+df=x*y*dx+y*z*dy+z*x*dz
+df=simplify(df)
+% -(dt*sin(2*t)*(sin(t) - 1))/2
+I=int(df/dt,t,0,pi/2)
+% 1/6
 
