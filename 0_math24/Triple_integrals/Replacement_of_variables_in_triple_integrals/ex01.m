85c85feeb63a1966cc5835a6d321f250c16ad87e
Vital Teresh <vster2006@gmail.com>
2018-05-14 13:12:56 +0300

    Replacement_of_variables_in_triple_integrals add ex 01-02

---
+++ 0_math24/Triple_integrals/Replacement_of_variables_in_triple_integrals/ex01.m
@@ -0,0 +1,22 @@
+% Найти объем области U, заданной неравнествами
+% 0<=z<=2, 0<=y+z<=5, 0<=x+y+z<=10
+
+syms x y z;
+u=x+y+z
+v=y+z
+w=z
+% U1: 0<=u<=10, 0<=v<=5, 0<=w<=2
+
+% V = int int int (U) dx dy dz = int int int (U1) |I(u,v,w)| du dv dw
+
+% Вычислим Якобиан данного преобразования
+% d(u,v,w)/d(x,y,z) = ?
+I1=[diff(u,x) diff(u,y) diff(u,z); diff(v,x) diff(v,y) diff(v,z); diff(w,x) diff(w,y) diff(w,z)]
+dI1=det(I1)
+
+I=I1^(-1)
+dI=det(I)
+
+syms u v w;
+V=int(int(int(dI,w,0,2),v,0,5),u,0,10)
+% 100
---
+++ 0_math24/Triple_integrals/Replacement_of_variables_in_triple_integrals/ex02.m
@@ -0,0 +1,20 @@
+% Найти объем наклонного параллелепипеда, заданного неравенствами
+% 0<=2x-3y+z<=5, 1<=x+2y<=4, -3<=3-z<=6
+
+syms x y z;
+u=2*x-3*y+z
+v=x+2*y
+w=x-z
+
+% Вычислим Якобиан данного преобразования
+% I1=d(u,v,w)/d(x,y,z)
+I1=[diff(u,x) diff(u,y) diff(u,z); diff(v,x) diff(v,y) diff(v,z); diff(w,x) diff(w,y) diff(w,z)]
+dI1=det(I1)
+
+I=I1^(-1)
+dI=norm(det(I))
+
+syms u v w;
+V=int(int(int(dI,w,-3,6),v,1,4),u,0,5)
+% 15
+
