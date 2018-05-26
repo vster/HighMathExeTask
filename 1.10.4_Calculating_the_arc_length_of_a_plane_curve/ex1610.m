85b23a9d1e6b7e60849e3d9d77eccb3533e0d762
Vital Teresh <vster2006@gmail.com>
2018-05-26 13:57:56 +0300

    add ex 1610

--- 1.10.4_Calculating_the_arc_length_of_a_plane_curve/ex1610.m
+++ 1.10.4_Calculating_the_arc_length_of_a_plane_curve/ex1610.m
@@ -1,8 +1,12 @@
+% Find the arc length of the curve
+% y^2=x^3 from x=0 to x=1
+
 syms x y;
 f=y^2-x^3
 
-ezplot(f)
+% ezplot(f)
 
+% L = int (a-b) sqrt(1+diff(y)^2) dx
 dy=diff(x^(3/2))
-
 L=int(sqrt(1+dy^2),0,1)
+% (13*13^(1/2))/27 - 8/27
