8fd0bc5c0ebee90a3b4177b8d376894fdb9d340e
Vital Teresh <vster2006@gmail.com>
2018-05-27 23:47:37 +0300

    Surface_Integrals add ex 227

---
+++ 2.13.5_Surface_Integrals/ex227.m
@@ -0,0 +1,34 @@
+% Calculate 
+% I = int int (S) (x^2+y^2) dS
+% where S - part of the conical surface 
+% z^2=x^2+y^2
+% enclosed between the planes z=0 and z=1
+
+% I = int int (S) F(x,y,z) dS = 
+%   = int int (D) F(x,y,f(x,y))*sqrt(1+diff(z,x)^2+diff(z,y)^2)*dx*dy
+syms x y dx dy real;
+z=sqrt(x^2+y^2)
+dzx=diff(z,x)
+dzy=diff(z,y)
+dS=sqrt(1+dzx^2+dzy^2)
+dS=simplify(dS)*dx*dy
+% 2^(1/2)*dx*dy
+% I = int int (D) (x^2+y^2)*2^(1/2)*dx*dy
+f1=(x^2+y^2)*2^(1/2)
+% D: curcle x^2+y^2<=1
+f2=x^2+y^2-1
+
+syms ro teta;
+x1=ro*cos(teta)
+y1=ro*sin(teta)
+
+f1=subs(f1,[x y],[x1 y1])
+f1=simplify(f1)
+% 2^(1/2)*ro^2
+
+f2=subs(f2,[x y],[x1 y1])
+f2=simplify(f2)
+%  ro<=1
+
+I=4*int(int(f1*ro,ro,0,1),teta,0,pi/2)
+% (pi*2^(1/2))/2
