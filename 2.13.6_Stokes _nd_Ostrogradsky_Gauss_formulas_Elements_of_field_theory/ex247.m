d8dc634c52874229fa29aca605003444b97dc8d9
Vital Teresh <vster2006@gmail.com>
2018-06-02 16:38:08 +0300

    modify ex 247

--- 2.13.6_Stokes _nd_Ostrogradsky_Gauss_formulas_Elements_of_field_theory/ex247.m
+++ 2.13.6_Stokes _nd_Ostrogradsky_Gauss_formulas_Elements_of_field_theory/ex247.m
@@ -1,10 +1,11 @@
 % The body rotates around the axis with a constant angular velocity of w.
 % Find the velocity vortex at an arbitrary point in the body.
 
-% rot(v)=rot(w*r), where r=x*i+y*j+z*k
+% rot_v=rot(w*r), where r=x*i+y*j+z*k
 syms x y z wx wy wz real;
 w=[wx wy wz]
 r=[x y z]
 v=cross(w,r)
-rot_v=curl(v,r)
+% [ wy*z - wz*y, wz*x - wx*z, wx*y - wy*x]
+rot_v=curl(v,[x y z])
 % rot_v = [2*wx,2*wy,2*wz] = 2*w
