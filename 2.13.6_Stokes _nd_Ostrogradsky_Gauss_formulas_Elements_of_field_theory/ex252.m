00f2dd497f888e17f5584f2f2fb4c5f20a386ce8
Vital Teresh <vster2006@gmail.com>
2018-06-06 23:30:01 +0300

    add ex 252-253

---
+++ 2.13.6_Stokes _nd_Ostrogradsky_Gauss_formulas_Elements_of_field_theory/ex252.m
@@ -0,0 +1,30 @@
+% Find the integral
+% int int (S) (x^3*cos(alfa)+y^3*cos(beta)+z^3*cos(gamma))*dS
+% taken over the surface of a sphere
+% x^2+y^2+z^2=a^2,
+% where alfa,beta,gamma - angles of the outer normal with the coordinate axes
+
+% Ostrogradsky-Gauss formula
+% int int (S) (P*cos_alfa+Q*cos_beta+R*cos_gamma)*dS = 
+% = int int int (T) (dP/dx+dQ/dy+dR/dz)*dx*dy*dz
+
+syms x y z;
+P=x^3
+Q=y^3
+R=z^3
+f=diff(P,x)+diff(Q,y)+diff(R,z)
+% 3*x^2 + 3*y^2 + 3*z^2
+
+% We proceed to spherical coordinates
+syms ro phi teta;
+x1=ro*cos(phi)*sin(teta)
+y1=ro*sin(phi)*sin(teta)
+z1=ro*cos(teta)
+J=ro^2*sin(teta)
+
+f1=subs(f,[x y z],[x1 y1 z1])
+f1=simplify(f1)
+% 3*ro^2
+
+I=int(int(int(f1*J,ro,0,a),teta,0,pi),phi,0,2*pi)
+% (12*pi*a^5)/5
---
+++ 2.13.6_Stokes _nd_Ostrogradsky_Gauss_formulas_Elements_of_field_theory/ex253.m
@@ -0,0 +1,29 @@
+% Find
+% int int(S)(z^2-y^2)*cos(alfa)+(x^2-y^2)*cos(beta)+(y^2-x^2)*cos(gamma))*dS
+% where S - the outer surface of the hemisphere
+% x^2+y^2+z^2=a^2 (z>=0)
+
+% Ostrogradsky-Gauss formula
+% int int (S) (P*cos_alfa+Q*cos_beta+R*cos_gamma)*dS = 
+% = int int int (T) (dP/dx+dQ/dy+dR/dz)*dx*dy*dz
+
+syms x y z;
+P=z^2-y^2
+Q=x^2-y^2
+R=y^2-x^2
+f=diff(P,x)+diff(Q,y)+diff(R,z)
+% -2*y
+
+% We proceed to spherical coordinates
+syms ro phi teta;
+x1=ro*cos(phi)*sin(teta)
+y1=ro*sin(phi)*sin(teta)
+z1=ro*cos(teta)
+J=ro^2*sin(teta)
+
+f1=subs(f,[x y z],[x1 y1 z1])
+f1=simplify(f1)
+% -2*ro*sin(phi)*sin(teta)
+
+I=int(int(int(f1*J,ro,0,a),teta,0,pi/2),phi,0,2*pi)
+% 0
