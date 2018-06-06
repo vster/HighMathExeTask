b891a51741540d908ec79d924c25510a21db3e1c
Vital Teresh <vster2006@gmail.com>
2018-06-06 13:40:42 +0300

    add ex 02

---
+++ 0_math24/Curvilinear_integrals/Curvilinear_integrals_of_the_first_kind/ex02.m
@@ -0,0 +1,17 @@
+% Compute the integral
+% int (C) y^2 ds,
+% where C is the arc of the circle x = a*cos(t), y = a*sin(t), 0?t?pi/2
+
+syms a t dt real;
+x=a*cos(t)
+y=a*sin(t)
+
+ds=sqrt(diff(x)^2+diff(y)^2)*dt
+ds=simplify(ds)
+% dt*abs(a)
+ds=a*dt
+
+df=y^2*ds
+% a^3*dt*sin(t)^2
+I = int(df/dt,t,0,pi/2)
+% (pi*a^3)/4
