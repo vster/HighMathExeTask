be7e2e974cf699412c246ce1b0c2cb54ec7eca50
Vital Teresh <vster2006@gmail.com>
2018-06-12 10:16:40 +0300

    Numerical_series theory

---
+++ 2.14.1_Numerical_series/theory.m
@@ -0,0 +1,34 @@
+% The first sign of comparison
+% Given 2 series u(n) and v(n)
+% u(n)<=v(n)
+% if u(n) is diverges => v(n) is diverges 
+% if v(n) is converges => u(n) is converges
+
+% The second sign of comparison
+% If exist lim(n->inf)u(n)/v(n)=k >0
+% then both series are converge or diverge
+
+% The Cauchy test
+% If for series u(n) exist lim(n->inf)u(n)^(1/n)=C
+% then if C<1 then series is converges
+%      if C>1 then series is diverges
+
+% d'Alembert's test
+% If for series u(n) exist lim(n->inf)u(n+1)/u(n)=D
+% then if D<1 then series is converges
+%      if D>1 then series is diverges
+
+% Integral test
+% Given function f(x) on x>=1
+% S=sum(n=1:inf)u(n), where u(n)=f(x)
+% I=int(N:inf)f(x)dx
+% if I is converges then S is converges
+% if I is diverges then S is diverges 
+
+% The sign of convergence of the alternating series
+% Leibniz sign
+% Series is converges if 
+% the absolute values of its members
+% 1) u1>u2>u3>...
+% 2) lim(n->inf)u(n)=0
+
