cb9f6200a4943a353c4b3b473fe053ae900d23c1
Vital Teresh <vster2006@gmail.com>
2018-06-20 19:46:28 +0300

    add ex 313-315

--- 2.14.1_Numerical_series/ex314.m
+++ 2.14.1_Numerical_series/ex314.m
@@ -5,3 +5,21 @@
 clear
 syms n;
 digits(4)
+u(n)=(-1)^ceil(2/3*(n+1))*(1+6*(n-1))/10^n
+
+% for n=1:10
+%    s(n)=u(n)
+% end
+
+% Use the Leibniz test
+% 1 condition
+for n=1:6
+     a=vpa(abs(u(n)))
+end
+% 0.1>0.07>0.013>0.0019>0.00025>3.1e-5
+
+% 2 condition
+syms n;
+L=limit(abs(u(n)),n,inf)
+% 0
+% L=0 => series is converge
--- 2.14.1_Numerical_series/ex315.m
+++ 2.14.1_Numerical_series/ex315.m
@@ -1,23 +1,25 @@
 % Investigate the convergence of alternating series 
 % and establish the nature of convergence (absolute, conditional)
-% sum(n=1:inf)(-1)^(n-1)*(n+1)/(n^2+n+1)
+% (3+1/2)+(3+1/4)-(3+1/8)-(3+1/16)+(3+1/32)+(3+1/64)-...
 
 clear
 syms n;
 digits(4)
-u(n)=(-1)^(n-1)*(n+1)/(n^2+n+1)
+u(n)=(-1)^(ceil(1/2*n)+1)*(3+1/2^n)
+
+% for n=1:10
+%    s(n)=u(n)
+% end
+
 % Use the Leibniz test
-
-S=symsum(u(n),n,1,inf)
-
 % 1 condition
 for n=1:6
      a=vpa(abs(u(n)))
 end
-% 0.6667>0.4286>0.3077>0.2381>0.1935>0.1628>...
+% 3.5>3.25>3.125>3.062>3.031>3.016
 
 % 2 condition
 syms n;
 L=limit(abs(u(n)),n,inf)
-% 0
-% L=0 => series is converge
+% 3
+% L>0 => series is diverges
