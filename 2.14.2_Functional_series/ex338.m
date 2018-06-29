488cccbf34ec159e7eb5086be0a424765142b97f
Vital Teresh <vster2006@gmail.com>
2018-06-29 20:39:46 +0300

    add ex 338-339

---
+++ 2.14.2_Functional_series/ex338.m
@@ -0,0 +1,16 @@
+% Given a functional range
+% u(x,n)=1/(2*n-1)*((4-x)/(7*x+2))^n
+% Investigate for convergence at points x=0 and x=1.
+
+syms x n;
+u(x,n)=1/(2*n-1)*((4-x)/(7*x+2))^n
+
+x=0
+u0(n)=u(x,n)
+D=limit(u0(n+1)/u0(n),n,inf)
+% 2 > 1 => series is diverges
+
+x=1
+u1(n)=u(x,n)
+D=limit(u1(n+1)/u1(n),n,inf)
+% 1/3 < 1 => series is converge
---
+++ 2.14.2_Functional_series/ex339.m
@@ -0,0 +1,28 @@
+% Find the domain of convergence of the series
+% u(x,n)=1/(1+x^(2*n))
+
+syms x n;
+u(x,n)=1/(1+x^(2*n))
+
+assume(-1<x<1)
+L1=limit(u(x,n),n,inf)
+% L1=1 > 0 => series is diverge
+
+assume(x==1)
+L2=limit(u(x,n),n,inf)
+% L2=1/2 > 0 => series is diverge
+
+assume(x==-1)
+L3=limit(u(x,n),n,inf)
+% L3=NaN => series is diverge
+
+assume(x>1)
+L4=limit(u(x,n),n,inf)
+% L4=0 => series is converge
+
+assume(x<-1)
+L5=limit(u(x,n),n,inf)
+% L5=0 => series is converge
+
+% if |x|>1 => series is converge
+
