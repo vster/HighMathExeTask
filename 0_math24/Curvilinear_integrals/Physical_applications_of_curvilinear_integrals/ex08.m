317b23bca9f5f222d0266c33c9180477aa559406
Vital Teresh <vster2006@gmail.com>
2018-07-05 19:51:00 +0300

    translate units

--- 0_math24/Curvilinear_integrals/Physical_applications_of_curvilinear_integrals/ex08.m
+++ 0_math24/Curvilinear_integrals/Physical_applications_of_curvilinear_integrals/ex08.m
@@ -14,15 +14,15 @@ d_ksi=2*r*B*v*dt
 Eps=-d_ksi/dt
 % -2*B*r*v
 Eps=2*B*r*v
-% v = 900 ךל/ק = 250 ל/ס
-% r = 1 סל = 0.01 ל
-% B = 5E-5 Ò
+% v = 900 km/h = 250 m/s
+% r = 1 cm = 0.01 m
+% B = 5E-5 T
 
 Eps1=subs(Eps,[B,r,v],[5E-5,0.01,250])
 % 1/4000
 digits(2)
 vpa(Eps1)
-% Eps = 2.5e-4 Â
+% Eps = 2.5e-4 V
 
 % The strength of the resulting electric field
 % Eps = int_c(C)E*dr = int_c(C)E*dr*cos(0) = E*int_c(C)*dr = 2*pi*r*E
@@ -31,4 +31,4 @@ E = Eps/(2*pi*r)
 E=subs(E,[Eps,r],[Eps1,0.01])
 % 1/(80*pi)
 vpa(E)
-% E = 4.0e-3 Â/ל
+% E = 4.0e-3 V/m
