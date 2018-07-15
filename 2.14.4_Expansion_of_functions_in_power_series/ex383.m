e11fb3ea16ed35c2b299db97e2669737af009ee6
Vital Teresh <vster2006@gmail.com>
2018-07-15 17:23:51 +0300

    modify ex 383

--- 2.14.4_Expansion_of_functions_in_power_series/ex383.m
+++ 2.14.4_Expansion_of_functions_in_power_series/ex383.m
@@ -12,16 +12,31 @@ for n=1:7
     df(n)=diff(f(x),x,n);
     df0(n)=subs(df(n),x,x0);
     ts=ts+df0(n)/factorial(n)*x^n;
-    ezplot(ts,[-1 1])
-    grid on
+    % ezplot(ts,[-1 1])
+    % grid on
 end
 df
 df=simplify(df)
 % [ sin(2*x), 2 - 4*sin(x)^2, -4*sin(2*x), 16*sin(x)^2 - 8, 16*sin(2*x), 32 - 64*sin(x)^2, -64*sin(2*x)]
+% df(x,n)=2^(n-1)*sin(2*x+pi/2*(n-1))
+% df(x,n+1)=2^n*sin(2*x+pi/2*n)
 df0
 % [ 0, 2, 0, -8, 0, 32, 0]
 ts
 % (2*x^6)/45 - x^4/3 + x^2
+syms x n c;
+% Remainder
+% R(n)=2^n*sin(2*c+pi*n/2)/factorial(n+1)*x^(n+1)
+R(n)=1/2*(2*x)^(n+1)/factorial(n+1)*sin(2*c+pi*n/2)
+R1(n)=2^n/factorial(n)
+L=limit(R1,n,inf)
+% 0 => limit(R(n),n,inf)=0
 
 ts1=taylor(f(x),'Order',8)
+% (2*x^6)/45 - x^4/3 + x^2
+
+% sin(x)^2=1/2*(1-cos(2*x))
+ts2=taylor(cos(2*x),'Order',8)
+% - (4*x^6)/45 + (2*x^4)/3 - 2*x^2 + 1
+ts3=1/2*(1-ts2)
 % (2*x^6)/45 - x^4/3 + x^2
