c984bc40698e8644926b647590a7cdcfbe6e8d5d
Vital Teresh <vster2006@gmail.com>
2018-07-27 09:39:22 +0300

    modify disp

--- 2.14.5_Approximate_computations_of_the_values_of_functions_by_means_of_power_series/ex404.m
+++ 2.14.5_Approximate_computations_of_the_values_of_functions_by_means_of_power_series/ex404.m
@@ -11,20 +11,21 @@ R(x,n)=x^n/factorial(n)*(x/(n+1-x))
 R1(n)=subs(R,x,1/2)
 % (1/2)^n/(2*factorial(n)*(n + 1/2))
 
-s=0
 syms i;
+s=0;
 for i=0:7
-    i
-    term=subs(u(x,n),[x n],[1/2 i])
-    vpa(term)
-    s=s+term
-    vpa(s)
-    R1(i)
+    term=subs(u(x,n),[x n],[1/2 i]);
+    vpa(term);
+    s=s+term;
+    vpa(s);
+    R1(i);
+    disp([i term vpa(term) s vpa(s) R1(i) vpa(R1(i))])
     if (R1(i)<err)
         break
     end
 end
 s1=vpa(s)
+% 1.6487196
 
 
 
