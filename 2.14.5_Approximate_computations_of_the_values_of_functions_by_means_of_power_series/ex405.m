6ecd82c9e81c27bf6a6e85eb9298ed6492b5774d
Vital Teresh <vster2006@gmail.com>
2018-07-28 10:10:37 +0300

    add ex 405-406

---
+++ 2.14.5_Approximate_computations_of_the_values_of_functions_by_means_of_power_series/ex405.m
@@ -0,0 +1,32 @@
+% Calculate 1/exp(-1/5) accurate to 0.00001
+
+syms x n;
+% exp(x) = sum (n=0:inf) u(x,n)
+f(x)=exp(x)
+x0=-1/5
+u(x,n)=x^n/factorial(n)
+digits(8)
+err=0.00001
+
+R(x,n)=abs(x^(n+1)/factorial(n+1))
+R1(n)=subs(R,x,x0)
+% (-1/5)^(n + 1)/factorial(n + 1)
+
+syms i;
+s=0;
+for i=0:7
+    term=subs(u(x,n),[x n],[x0 i]);
+    vpa(term);
+    s=s+term;
+    vpa(s);
+    R1(i);
+    disp([i term vpa(term) s vpa(s) R1(i) vpa(R1(i))])
+    if (R1(i)<err)
+        break
+    end
+end
+s1=vpa(s)
+% 0.81873333
+
+
+
---
+++ 2.14.5_Approximate_computations_of_the_values_of_functions_by_means_of_power_series/ex406.m
@@ -0,0 +1,29 @@
+% Calculate cos(18grad) accurate to 0.0001
+
+syms x n;
+% cos(x) = sum (n=0:inf) u(x,n)
+f(x)=cos(x)
+x0=pi/10
+u(x,n)=(-1)^(n-1)*x^(2*(n-1))/factorial(2*(n-1))
+digits(8)
+err=0.0001
+
+R(x,n)=abs(u(x,n+1))
+R1(n)=subs(R,x,x0)
+% abs((-1)^n*(pi/10)^(2*n))/factorial(2*n)
+
+syms i;
+s=0;
+for i=1:7
+    term=subs(u(x,n),[x n],[x0 i]);
+    vpa(term);
+    s=s+term;
+    vpa(s);
+    R1(i);
+    disp([i term vpa(term) s vpa(s) R1(i) vpa(R1(i))])
+    if (R1(i)<err)
+        break
+    end
+end
+s1=vpa(s)
+% 0.95105785
