db41ca3e7522c906296aa0210b2ecef805a042dc
Vital Teresh <vster2006@gmail.com>
2018-07-28 12:22:08 +0300

    modify ex 383-384

--- 2.14.4_Expansion_of_functions_in_power_series/ex383.m
+++ 2.14.4_Expansion_of_functions_in_power_series/ex383.m
@@ -14,6 +14,7 @@ for n=1:7
     df(n)=diff(dfp,x);
     df0(n)=subs(df(n),x,x0);
     ts=ts+df0(n)/factorial(n)*(x-x0)^n;
+    disp([df(n) df0(n) ts])
     % ezplot(ts,[-1 1])
     % grid on
     dfp=df(n);
@@ -51,10 +52,12 @@ f0=subs(f(x1),x1,x0)
 ts=f0
 term=f
 for n=1:7
-    dterm=diff(term,x1)
-    term=dterm*(x-x0)/n
-    term0=subs(term,x1,x0)
-    ts=ts+term0
+    dterm=diff(term,x1);
+    dterm=simplify(dterm);
+    term=dterm*(x-x0)/n;
+    term0=subs(term,x1,x0);
+    ts=ts+term0;
+    disp([dterm term term0 ts])
 end
 ts4=ts
 % (2*x^6)/45 - x^4/3 + x^2
--- 2.14.4_Expansion_of_functions_in_power_series/ex384.m
+++ 2.14.4_Expansion_of_functions_in_power_series/ex384.m
@@ -11,8 +11,10 @@ ts=f0;
 dfp=f;
 for n=1:6
     df(n)=diff(dfp,x);
+    df(n)=simplify(df(n));
     df0(n)=subs(df(n),x,x0);
     ts=ts+df0(n)/factorial(n)*(x-x0)^n;
+    disp([df(n) df0(n) ts])
     % ezplot(ts,[-2 2])
     % hold on
     % grid on
@@ -35,11 +37,13 @@ f0=subs(f(x1),x1,x0)
 ts=f0
 term=f
 for n=1:7
-    dterm=diff(term,x1)
-    term=dterm*(x-x0)/n
+    dterm=diff(term,x1);
+    dterm=simplify(dterm);
+    term=dterm*(x-x0)/n;
     % term=simplify(term)
-    term0=subs(term,x1,x0)
-    ts=ts+term0
+    term0=subs(term,x1,x0);
+    ts=ts+term0;
+    disp([dterm term term0 ts])
 end
 ts2=ts
 % - x^6/6 + x^4/2 - x^2 + 1
