65733967f158e8e3d37dbbb00c5c1bd28d6bbafd
Vital Teresh <vster2006@gmail.com>
2018-07-30 00:33:45 +0300

    modify ex 404-405

--- 2.14.5_Approximate_computations_of_the_values_of_functions_by_means_of_power_series/ex404.m
+++ 2.14.5_Approximate_computations_of_the_values_of_functions_by_means_of_power_series/ex404.m
@@ -15,10 +15,7 @@ syms i;
 s=0;
 for i=0:7
     term=subs(u(x,n),[x n],[1/2 i]);
-    vpa(term);
     s=s+term;
-    vpa(s);
-    R1(i);
     disp([i term vpa(term) s vpa(s) R1(i) vpa(R1(i))])
     if (R1(i)<err)
         break
@@ -27,5 +24,35 @@ end
 s1=vpa(s)
 % 1.6487196
 
+clear
+syms x n;
+f=exp(x);
+digits(8);
+err=0.00001;
 
+x0=0;
+x1=1/2;
+for n=1:10
+    if n>1
+        df(n)=diff(f,x,n-1);
+    else
+        df(n)=f;
+    end
+    df0(n)=subs(df(n),x,x0);
+    term(n)=df0(n)/factorial(n-1)*(x-x0)^(n-1);
+    disp([n,df(n),df0(n),term(n)])
+end
+term
+% [ 1, x, x^2/2, x^3/6, x^4/24, x^5/120]
+term1=subs(term,x,x1)
+% [ 1, 1/2, 1/8, 1/48, 1/384, 1/3840, 1/46080, 1/645120, 1/10321920, 1/185794560]
 
+s2=0;
+for n=1:10
+    s2=s2+term1(n);
+    if abs(term1(n+1))<err
+        break
+    end
+end
+vpa(s2)
+% 1.6487196
--- 2.14.5_Approximate_computations_of_the_values_of_functions_by_means_of_power_series/ex405.m
+++ 2.14.5_Approximate_computations_of_the_values_of_functions_by_means_of_power_series/ex405.m
@@ -25,5 +25,35 @@ end
 s1=vpa(s)
 % 0.81873333
 
+clear
+syms x n;
+f=exp(x);
+digits(8);
+err=0.00001;
+x0=0;
+x1=-1/5;
+for n=1:8
+    if n>1
+        df(n)=diff(f,x,n-1);
+    else
+        df(n)=f;
+    end
+    df0(n)=subs(df(n),x,x0);
+    term(n)=df0(n)/factorial(n-1)*(x-x0)^(n-1);
+    disp([n,df(n),df0(n),term(n)])
+end
+term
+% [ 1, x, x^2/2, x^3/6, x^4/24, x^5/120, x^6/720, x^7/5040]
+term1=subs(term,x,x1)
+% [ 1, -1/5, 1/50, -1/750, 1/15000, -1/375000, 1/11250000, -1/393750000]
 
+s2=0
+for n=1:8
+    s2=s2+term1(n);
+    if abs(term1(n+1))<err
+        break
+    end
+end
+vpa(s2)
+% 0.81873333
 
