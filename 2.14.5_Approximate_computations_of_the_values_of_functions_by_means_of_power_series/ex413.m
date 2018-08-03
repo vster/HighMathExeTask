8b0703d76226317dfee569dde275d3d87d571332
Vital Teresh <vster2006@gmail.com>
2018-08-04 00:12:13 +0300

    add ex 412-424

---
+++ 2.14.5_Approximate_computations_of_the_values_of_functions_by_means_of_power_series/ex413.m
@@ -0,0 +1,26 @@
+% Calculate log(2) accurate to 0.0001
+
+% log(5) = log(4+1)
+
+clear
+syms t n;
+% log(t+1) = log(t) + sum (n=0:inf) u(t,n)
+t0=4
+u(t,n)=1/((2*n-1)*(2*t+1)^(2*n-1))
+digits(8)
+err=0.0001
+
+syms i;
+s=log(t0);
+for i=1:7
+    term=2*subs(u(t,n),[t n],[t0 i]);
+    R1(i)=2*subs(u(t,n),[t n],[t0 i+1]);
+    s=s+term;
+    disp([i term vpa(term) s vpa(s) R1(i) vpa(R1(i))])
+    if (R1(i)<err)
+        break
+    end
+end
+s1=vpa(s)
+% 1.6094311
+
