c086c5294b1562a18a6e9f7103a746b72631c889
Vital Teresh <vster2006@gmail.com>
2018-08-12 14:06:41 +0300

    modify ex 485-486

--- 2.14.8_Fourier_series/ex485.m
+++ 2.14.8_Fourier_series/ex485.m
@@ -15,21 +15,39 @@ assume(m>=1)
 a(m)=int(f(x)*cos(m*x),x,-pi,pi)/pi
 % (2*sin(pi*m))/m
 a(m)=0
+af(m)=a(m)*cos(m*x);
 
 b(m)=int(f(x)*sin(m*x),x,-pi,pi)/pi
 % b(m)=(2*(sin(pi*m) - m*pi*cos(pi*m)))/(m^2*pi)
 b(m)=2*(-m*pi*cos(pi*m))/(m^2*pi)
 % -(2*cos(pi*m))/m
 b(m)=2/m*(-1)^(m+1)
-
-bt(m)=b(m)*sin(m*x)
+bf(m)=b(m)*sin(m*x)
 % (2*(-1)^(m + 1)*sin(m*x))/m
+abf(m)=af(m)+bf(m);
+% (2*(-1)^(m + 1)*sin(m*x))/m
+
+fplot(f(x),[-pi pi]);
+hold on;
 
 f1=a0/2;
 for m=1:5
-    f1=f1+bt(m);
+    m1(m)=m;
+    a1(m)=a(m);
+    b1(m)=b(m);
+    abf1(m)=abf(m);
+    f1=f1+abf(m);
     fplot(f1,[-pi pi])
-    hold on
     grid on
 end
 hold off
+
+a0/2
+%
+ft=[m1;a1;b1;abf1];
+ft'
+% [ 1, 0,    2,       2*sin(x)]
+% [ 2, 0,   -1,      -sin(2*x)]
+% [ 3, 0,  2/3, (2*sin(3*x))/3]
+% [ 4, 0, -1/2,    -sin(4*x)/2]
+% [ 5, 0,  2/5, (2*sin(5*x))/5]
--- 2.14.8_Fourier_series/ex486.m
+++ 2.14.8_Fourier_series/ex486.m
@@ -1,27 +1,53 @@
 % Expand the Fourier series of a periodic function f(x) with period 2, 
 % given in the interval (-1;1) by the equation f(x)=x^2.
 
+
 syms x pi;
 f(x)=x^2;
-l=1;
+L=1;
 
-a0=(2/l)*int(f(x),x,0,1)
+a0=(2/L)*int(f(x),x,0,1)
 % 2/3
 
 syms m integer;
 assume(m>=1)
-a(m)=(2/l)*int(f(x)*cos(m*pi*x/l),x,0,1)
+a(m)=(2/L)*int(f(x)*cos(m*pi*x/L),x,0,1)
 % (2*(m^2*pi^2*sin(pi*m) - 2*sin(pi*m) + 2*m*pi*cos(pi*m)))/(m^3*pi^3)
 %                =0             =0               =(-1)^m 
 % a(m)=4*m*pi*(-1)^m/(m^3*pi^3)
 a(m)=4*(-1)^m/(m^2*pi^2)
+af(m)=a(m)*cos(m*pi*x/L)
+% (4*(-1)^m*cos(pi*m*x))/(m^2*pi^2)
+
 % f(x)-even => b(m)=0
+b(m)=sym(0)
+bf(m)=b(m)*sin(m*pi*x/L)
+% 0
+abf(m)=af(m)+bf(m)
+% (4*(-1)^m*cos(m*x))/(m^2*pi^2)
+
+fplot(f(x),[-1 1]);
+hold on;
 
 f1=a0/2;
 for m=1:5
-    f1=f1+a(m)*cos(m*pi*x);
+    m1(m)=m;
+    a1(m)=a(m);
+    b1(m)=b(m);
+    abf1(m)=abf(m);
+    f1=f1+abf(m);
     fplot(f1,[-1 1]);
-    hold on
     grid on
 end
 hold off
+
+a0/2
+% 1/3
+ft=[m1;a1;b1;abf1];
+ft'
+% [ 1,      -4/pi^2, 0,        -(4*cos(pi*x))/pi^2]
+% [ 2,       1/pi^2, 0,           cos(2*pi*x)/pi^2]
+% [ 3,  -4/(9*pi^2), 0,  -(4*cos(3*pi*x))/(9*pi^2)]
+% [ 4,   1/(4*pi^2), 0,       cos(4*pi*x)/(4*pi^2)]
+% [ 5, -4/(25*pi^2), 0, -(4*cos(5*pi*x))/(25*pi^2)] 
+
