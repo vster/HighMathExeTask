80b9a113acf0a8ff3699294930a4a398e78c6eb2
Vital Teresh <vster2006@gmail.com>
2018-08-29 19:24:04 +0300

    add ex 515

---
+++ 2.15.1_Differential_equations_of_the_first_order/ex515.m
@@ -0,0 +1,17 @@
+% log(cos(y))dx+x*tan(y)dy=0
+% dx/x+tan(y)*dy/log(cos(y))=0
+
+% 1)
+syms x y;
+eq1=int(1/x,x)+int(tan(y)/log(cos(y)),y)
+% log(log(cos(y))) = log(x) + log(C)
+% log(cos(y))=C*x
+% y=acos(exp(C*x))
+
+% 2)
+% log(cos(y))dx+x*tan(y)dy=0
+% dy/dx = -log(cos(y))/(x*tan(y))
+syms x y(x);
+eqn = diff(y(x)) == -log(cos(y))/(x*tan(y))
+ySol(x) = dsolve(eqn)
+% acos(exp(C5*x))
