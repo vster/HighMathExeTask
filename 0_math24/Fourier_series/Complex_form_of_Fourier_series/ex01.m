9f438c550521c6c54e7f5cc53ba314837624f61f
Vital Teresh <vster2006@gmail.com>
2018-08-30 09:41:15 +0300

    modify ex01

--- 0_math24/Fourier_series/Complex_form_of_Fourier_series/ex01.m
+++ 0_math24/Fourier_series/Complex_form_of_Fourier_series/ex01.m
@@ -20,47 +20,38 @@ pretty(c(n))
 % 1i/(pi*n)*((exp(-pi*n*1i)+exp(pi*n*1i))/2-1)
 % 1i/(pi*n)*((-1)^n-1)
 c(n)=1i/(pi*n)*((-1)^n-1)
+cf(n)=c(n)*exp(i*n*x)
+% (exp(n*x*1i)*((-1)^n - 1)*1i)/(n*pi)
 
-for n=1:7
+fplot(f(x),[-pi1,pi1])
+hold on
+grid on
+
+f1=sym(0);
+for n=1:10
     n1(n)=sym(n);
     c1(n)=c(n);
+    cn1(n)=c(-n);
+    cf1(n)=cf(n);
+    cfn1(n)=cf(-n);
+    abf1(n)=simplify(cf1(n)+cfn1(n));
+    f1=f1+abf1(n);
+    if (n==3 || n==10)
+        fplot(f1,[-pi1 pi1]);
+    end
+    grid on    
 end
-ft=[n1;c1];
+hold off
+
+ft=[n1;c1;cn1;cf1;cfn1;abf1];
 c0/2
 % 0
 ft'
-% [ 1,     2i/pi]
-% [ 2,         0]
-% [ 3, 2i/(3*pi)]
-% [ 4,         0]
-% [ 5, 2i/(5*pi)]
-% [ 6,         0]
-% [ 7, 2i/(7*pi)]
+% [ 1,     2i/pi,     -2i/pi,     (exp(-x*1i)*2i)/pi,     -(exp(x*1i)*2i)/pi,       (4*sin(x))/pi]
+% [ 2,         0,          0,                      0,                      0,                   0]
+% [ 3, 2i/(3*pi), -2i/(3*pi), (exp(-x*3i)*2i)/(3*pi), -(exp(x*3i)*2i)/(3*pi), (4*sin(3*x))/(3*pi)]
+% [ 4,         0,          0,                      0,                      0,                   0]
+% [ 5, 2i/(5*pi), -2i/(5*pi), (exp(-x*5i)*2i)/(5*pi), -(exp(x*5i)*2i)/(5*pi), (4*sin(5*x))/(5*pi)]
 
-syms n k;
-c(k)=subs(c(n),n,2*k-1)
-% (((-1)^(2*k - 1) - 1)*1i)/(pi*(2*k - 1))
-c(k)=-2*i/(pi*(2*k - 1))
-fc(k)=c(k)*exp(i*(2*k-1)*x)
-% -(exp(x*(k*2i - 1i))*2i)/(pi*(2*k - 1))
-for k=1:5
-    k2(k)=sym(k);
-    c2(k)=c(k);
-    fc2(k)=fc(k);
-end
-ft2=[k2;c2;fc2];
-c0/2
-% 0
-ft2'
-% [ 1,     2i/pi,     (exp(-x*1i)*2i)/pi]
-% [ 2, 2i/(3*pi), (exp(-x*3i)*2i)/(3*pi)]
-% [ 3, 2i/(5*pi), (exp(-x*5i)*2i)/(5*pi)]
-% [ 4, 2i/(7*pi), (exp(-x*7i)*2i)/(7*pi)]
-% [ 5, 2i/(9*pi), (exp(-x*9i)*2i)/(9*pi)]
 
-ft3=c0;
-for k=1:3
-    ft3=ft3+fc(k)+fc(-k);
-end
-ft3=simplify(ft3)
-% (2*exp(-x*7i)*(exp(x*2i)*21i + exp(x*4i)*35i - exp(x*8i)*105i - exp(x*10i)*35i - exp(x*12i)*21i + 15i))/(105*pi)
+
