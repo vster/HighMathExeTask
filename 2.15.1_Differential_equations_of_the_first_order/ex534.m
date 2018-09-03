2fba016d70632c472e3af63a746a90ddfa2134f9
Vital Teresh <vster2006@gmail.com>
2018-09-03 23:30:19 +0300

    modify ex534

--- 2.15.1_Differential_equations_of_the_first_order/ex534.m
+++ 2.15.1_Differential_equations_of_the_first_order/ex534.m
@@ -9,3 +9,10 @@ clear
 syms x y;
 eq1=int(sqrt(1+sin(y)),y)+int(sqrt(1+cos(2*x)),x)
 % (2*(sin(y) - 1)*(sin(y) + 1)^(1/2))/cos(y) + (2^(1/2)*sin(2*x))/(2*(cos(x)^2)^(1/2))
+
+% 2)
+clear
+syms x y(x);
+eqn=diff(y(x),x)==-sqrt((1+cos(2*x))/(1+sin(y)))
+ySol=dsolve(eqn)
+
