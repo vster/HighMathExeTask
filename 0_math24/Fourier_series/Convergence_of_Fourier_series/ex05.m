04f2b8b2ec2aa7544b340b67c05eccd4680d931f
Vital Teresh <vster2006@gmail.com>
2018-09-03 15:04:26 +0300

    ??? not correct ???

--- 0_math24/Fourier_series/Convergence_of_Fourier_series/ex05.m
+++ 0_math24/Fourier_series/Convergence_of_Fourier_series/ex05.m
@@ -1,5 +1,14 @@
 % It is known that the Fourier series of the function 
-% f (x) =(pi-x)/2
+% f(x) =(pi-x)/2
 % given in the interval [0,2?], is expressed by the formula 
 % f(x) =(pi-x)/2=sun(n=1:inf)sin(n*x)/n
 % Investigate the behavior of the partial sums fN (x).
+
+syms x n;
+f(x)=(pi-x)/2;
+abf(n)=sin(n*x)/n
+
+% f(x,N)=sum(n=1:N)sin(n*x)/n
+% f(x,N)=int(0:x)(sum(n=1:N)cos(n*t))dt
+% ...
+% ???
