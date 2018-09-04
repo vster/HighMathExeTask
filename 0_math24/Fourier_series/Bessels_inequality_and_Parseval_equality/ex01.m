c57835dcedb7c0a73e110e5cbd14cfeb672d62f0
Vital Teresh <vster2006@gmail.com>
2018-09-04 10:08:54 +0300

    Bessels_inequality_and_Parseval_equality add ex 01

---
+++ 0_math24/Fourier_series/Bessels_inequality_and_Parseval_equality/ex01.m
@@ -0,0 +1,21 @@
+% Calculate the sum of the series sum(n=1:inf)1/n^2
+% Note: Apply Parseval's formula to the function f(x) = x.
+
+% Parseval's formula
+% a0^2/2+sum(n=1:inf)(a(n)^2+b(n)^2)=1/pi*int(-pi:pi)f(x)^2*dx
+
+pi1=pi
+syms x n pi;
+f(x)=x
+b(n)=2/n*(-1)^(n+1)
+abf(n)=(2*(-1)^(n + 1)*sin(n*x))/n
+
+S=symsum(b(n)^2,n,1,inf)
+% (2*pi^2)/3
+I=1/pi*int(f(x)^2,x,-pi,pi)
+% (2*pi^2)/3
+
+% sum(n=1:inf)(2/n*(-1)^(n+1)))^2=1/pi=1/pi*int(-pi:pi)x^2*dx =>
+% 4*sum(n=1:inf)1/n^2=(2*pi^2)/3 =>
+% sum(n=1:inf)1/n^2=pi^2/6
+% sum(n=1:inf)1/n^2 is the Riemann zeta function
