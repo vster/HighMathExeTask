a1f0dc48a8ca8dcb92ac58146642feaf9754d3cf
Vital Teresh <vster2006@gmail.com>
2018-09-04 13:52:22 +0300

    add ex 02

---
+++ 0_math24/Fourier_series/Bessels_inequality_and_Parseval_equality/ex02.m
@@ -0,0 +1,21 @@
+% Apply Parseval's formula to the function f(x) = x^2.
+
+syms x n;
+f(x)=x^2
+
+a0=(2*pi^2)/3
+a(n)=2*2*n*pi*(-1)^n/(n^3*pi)
+b(n)=0
+abf(n)=(4*(-1)^n*cos(n*x))/n^2
+
+S=a0^2/2+symsum(a(n)^2,n,1,inf)
+% (2*pi^4)/5
+I=1/pi*int(f(x)^2,x,-pi,pi)
+% (2*pi^4)/5
+
+% Parseval's formula
+% a0^2/2+sum(n=1:inf)(a(n)^2+b(n)^2)=1/pi*int(-pi:pi)f(x)^2*dx
+% ((2*pi^2)/3)^2+sum(n=1:inf)(2*2*n*pi*(-1)^n/(n^3*pi))^2=1/pi*int(-pi:pi)x^4*dx
+% 2*pi^4/9+16*sum(n=1:inf)1/n^4=2*pi^4/5
+% sum(n=1:inf)1/n^4=pi^4/90
+% sum(n=1:inf)1/n^4 is Riemann zeta function
