ec6c6d4c11f2543a84f515d734245c64509b4452
Vital Teresh <vster2006@gmail.com>
2018-10-09 21:09:57 +0300

    md ex 651

--- 2.15.2_Differential_Equations_of_Higher_Order/3_Differential_equations_of_the_form_F_x_dy_ not_containing_the_desired_function/ex651.m
+++ 2.15.2_Differential_Equations_of_Higher_Order/3_Differential_equations_of_the_form_F_x_dy_ not_containing_the_desired_function/ex651.m
@@ -10,7 +10,9 @@ eqn=diff(y,x,2)-diff(y,x)/(x-1)==x*(x-1)
 Dy(x)=diff(y,x)
 cond=[y(2)==1,Dy(2)==-1]
 ySol=dsolve(eqn,cond)
-% (x^3*(x - 2))/4 - (x^3*(3*x - 8))/24 - (3*x*(x - 2))/2 + 1/3                                  
+% (x^3*(x - 2))/4 - (x^3*(3*x - 8))/24 - (3*x*(x - 2))/2 + 1/3
+ySol=expand(ySol)
+% x^4/8 - x^3/6 - (3*x^2)/2 + 3*x + 1/3
 
 % Checking
 eqn10=diff(y,x,2)-diff(y,x)/(x-1)-x*(x-1)
