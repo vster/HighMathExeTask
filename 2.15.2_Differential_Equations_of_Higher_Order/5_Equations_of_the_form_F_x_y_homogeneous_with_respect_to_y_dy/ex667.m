66a96b81f681c490d80b03d6d08275414549a182
Vital Teresh <vster2006@gmail.com>
2018-10-13 09:15:49 +0300

    modify ex 667-668

--- 2.15.2_Differential_Equations_of_Higher_Order/5_Equations_of_the_form_F_x_y_homogeneous_with_respect_to_y_dy/ex667.m
+++ 2.15.2_Differential_Equations_of_Higher_Order/5_Equations_of_the_form_F_x_y_homogeneous_with_respect_to_y_dy/ex667.m
@@ -16,3 +16,11 @@ for i=1:length(ySol)
     chk(i)=simplify(subs(eqn10,y(x),ySol(i)))
 end
 % [ 0, 0, 0]
+
+% 2)
+% y'^2+y*y''=y*y' 
+% (y*y')'=y*y'
+% d(y*y')/(y*y')=dx
+% log(y*y')=x+C => y*y'=C1*exp(x)
+% y*dy=C1*exp(x)*dx
+% y^2/2=C1*exp(x)+C2
--- 2.15.2_Differential_Equations_of_Higher_Order/5_Equations_of_the_form_F_x_y_homogeneous_with_respect_to_y_dy/ex668.m
+++ 2.15.2_Differential_Equations_of_Higher_Order/5_Equations_of_the_form_F_x_y_homogeneous_with_respect_to_y_dy/ex668.m
@@ -15,3 +15,12 @@ for i=1:length(ySol)
     chk(i)=simplify(subs(eqn10,y(x),ySol(i)))
 end
 % [ 0, 0]
+
+% 2)
+% y*y''-y'^2=0
+% y''/y-y'^2/y^2=0
+% Put y'/y=z => y''/y-y'^2/y^2=z'
+% z'=0 => z=C1
+% y'=C1*y => dy/y=C1*dx => 
+% log(y)=C1*x+C2
+% y=exp(C1*x+C2)
