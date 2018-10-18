4d27ee27780039ec528ec68acea0b1446cae9cae
Vital Teresh <vster2006@gmail.com>
2018-10-18 21:00:05 +0300

    add ex 717-718

---
+++ 2.15.3_Higher_order_linear_equations/4_Linear_nonhomogeneous_equations/ex717.m
@@ -0,0 +1,19 @@
+% Find a solution to the equation 
+% y''+y=3*sin(x)
+% satisfying the initial conditions 
+% y(0)+y'(0)=0,y(pi/2)+y'(pi/2)=0
+
+% 1)
+syms x y(x)
+eqn=diff(y,x,2)+y==3*sin(x)
+dy(x)=diff(y)
+cond=[y(0)+dy(0)==0,y(pi/2)+dy(pi/2)==0]
+ySol=simplify(dsolve(eqn,cond))
+% (3*cos(x))/4 + (3*sin(x))/4 + (3*pi*cos(x))/8 - (3*pi*sin(x))/8 - (3*x*cos(x))/2
+
+% Checking
+eqn10=diff(y,x,2)+y-3*sin(x)
+for i=1:length(ySol)
+    chk(i)=simplify(subs(eqn10,y(x),ySol(i)))
+end
+% 0
