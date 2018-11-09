3230244b8268dc0029fbd701ac14ca0f054ba85e
Vital Teresh <vster2006@gmail.com>
2018-11-10 00:14:09 +0300

    2.16.14_The_theorem_of_Moivard_Laplace. add ex 929

---
+++ 2.16.14_The_theorem_of_Moivard_Laplace/a_theory.m
@@ -0,0 +1,6 @@
+% normalized frequency of random event
+% tau(n)=(m/n-p)/sqrt(p*q/n)
+
+% The theorem of Moivard-Laplace
+% P(a<(m/n-p)/sqrt(p*q/n)<b)=P(a<(m-n*p)/sqrt(n*p*q)<b) ~
+%    ~ 1/2*(erf(b/sqrt(2))-erf(a/sqrt(2))
---
+++ 2.16.14_The_theorem_of_Moivard_Laplace/ex929.m
@@ -0,0 +1,11 @@
+% a<(x-n*p)/sqrt(n*p*q)<b
+% n*p+a*sqrt(n*p*q)<x<n*p+b*sqrt(n*p*q)
+
+% Put n*p+a*sqrt(n*p*q)=alfa
+% n*p+b*sqrt(n*p*q)=beta
+% then
+% a=(alfa-n*p)/sqrt(n*p*q)
+% b=(beta-n*p)/sqrt(n*p*q)
+
+% Apply the theorem of Moivard-Laplace
+% P(alfa<X<beta)=1/2(erf((beta-n*p)/sqrt(2*n*p*q))-erf((alfa-n*p)/sqrt(2*n*p*q))
