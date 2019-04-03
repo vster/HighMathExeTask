b2c5787d99c0d3312b9b6e80f288e2c62d2c1155
Vital Teresh <vster2006@gmail.com>
2019-04-03 15:00:57 +0300

    modify ex 990-991

--- 2.17.3_String_oscillation_equation/ex990.m
+++ 2.17.3_String_oscillation_equation/ex990.m
@@ -4,10 +4,30 @@
 
 % a=1, psi(x)=0
 
+% 1)
 syms a x t phi(x)
 a=1
 phi(x)=x^2
 u=(subs(phi,x,x-a*t)+subs(phi,x,x+a*t))/2
 % (t - x)^2/2 + (t + x)^2/2
 u=simplify(u)
+% t^2 + x^2
+
+% 2) OperCalc
+syms t x p u(x,t)
+eq1=diff(u,t,2)-diff(u,x,2)
+eq2=laplace(eq1,t,p)
+% p^2*laplace(u(x, t), t, p) - p*u(x, 0) - subs(diff(u(x, t), t), t, 0) - laplace(diff(u(x, t), x, x), t, p)
+
+syms U(x)
+eq3=subs(eq2,[laplace(diff(u(x, t), x, x), t, p)...
+      laplace(u(x, t), t, p) u(x, 0) subs(diff(u(x, t), t), t, 0)],...
+      [diff(U(x),x,2) U(x) x^2 0])
+% p^2*U(x) - p*x^2 - diff(U(x), x, x)
+
+U1=dsolve(eq3)
+% x^2/p + C1*exp(-p*x) + C2*exp(p*x) + 2/p^3
+% C1=0, C2=0
+U1=x^2/p + 2/p^3
+u2=ilaplace(U2,p,t)
 % t^2 + x^2
--- 2.17.3_String_oscillation_equation/ex991.m
+++ 2.17.3_String_oscillation_equation/ex991.m
@@ -2,6 +2,7 @@
 % u(t=0)=0 
 % d2u/dt2(t=0)=x
 
+% 1)
 clear
 syms x 
 a=2
@@ -10,4 +11,23 @@ psi=x
 
 syms z t
 u=1/a^2*int(z,z,x-a*t,x+a*t)
+% t*x
+
+% 2) OperCalc
+syms t x p u(x,t)
+eq1=diff(u,t,2)-4*diff(u,x,2)
+eq2=laplace(eq1,t,p)
+% p^2*laplace(u(x, t), t, p) - p*u(x, 0) - subs(diff(u(x, t), t), t, 0) - 4*laplace(diff(u(x, t), x, x), t, p)
+
+syms U(x)
+eq3=subs(eq2,[laplace(diff(u(x, t), x, x), t, p)...
+      laplace(u(x, t), t, p) u(x, 0) subs(diff(u(x, t), t), t, 0)],...
+      [diff(U(x),x,2) U(x) 0 x])
+% U(x)*p^2 - x - 4*diff(U(x), x, x)
+
+U1=dsolve(eq3)
+% C3*exp(-(p*x)/2) + C4*exp((p*x)/2) + x/p^2
+% C3=0, C4=0
+U2=x/p^2
+u1=ilaplace(U2,p,t)
 % t*x
