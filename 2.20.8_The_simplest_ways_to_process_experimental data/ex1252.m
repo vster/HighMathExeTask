355613ebb80ab14ab530fd31ab98cc5012fb6b05
Vital Teresh <vster2006@gmail.com>
2019-06-22 16:59:18 +0300

    add ex ex1252-1254

---
+++ 2.20.8_The_simplest_ways_to_process_experimental data/ex1252.m
@@ -0,0 +1,20 @@
+clear
+digits(5)
+T=[273 283 288 293 313 333 353 373]
+S=[29.4 33.3 35.2 37.2 45.8 55.2 65.6 77.3]
+% delta=A*t^alfa-S
+% delta1=log(A)+alfa*log(T)-log(S)
+syms A alfa lgA t
+delta1=vpa(lgA+alfa*log10(T)-log10(S))
+eq1=sum(delta1(1:4))
+eq2=sum(delta1(5:8))
+[alfa lgA]=solve([eq1,eq2])
+% alfa =
+% 3.0932
+% lgA =
+% -6.0624
+A=10^lgA
+% A =
+% 8.661e-7
+S1=A*t^alfa
+% 8.661e-7*t^3.0932
---
+++ 2.20.8_The_simplest_ways_to_process_experimental data/ex1254.m
@@ -0,0 +1,22 @@
+clear
+digits(5)
+T=[53.92 26.36 14.0 6.99 4.28 2.75 1.85]
+S=[6.86 14.70 28.83 60.40 101.9 163.3 250.3]
+% delta=A*t^alfa-S
+% delta1=log(A)+alfa*log(T)-log(S)
+syms A alfa lgA t
+delta1=vpa(lgA+alfa*log10(T)-log10(S))
+eq1=sum(delta1(1:4))
+eq2=sum(delta1(5:7))
+[alfa lgA]=solve([eq1,eq2])
+% alfa =
+% -1.0662
+% lgA =
+% 2.682
+A=10^lgA
+% A =
+% 480.87
+S1=A*t^alfa
+% 480.87/t^1.0662
+delta2=A*T.^alfa-S
+% [ -0.0098452, -0.0083925, 0.014967, 0.089461, 0.14895, 0.24252, -0.73555]
