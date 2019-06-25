2d9262b6e9e515aa91015baeb3a13ca4ac357426
Vital Teresh <vster2006@gmail.com>
2019-06-25 23:26:25 +0300

    add ex 1262-1264

---
+++ 2.20.8_The_simplest_ways_to_process_experimental data/ex1262.m
@@ -0,0 +1,33 @@
+clear
+digits(3)
+syms x
+f(x)=sin(pi*x/2)
+a=0
+b=1
+syms a0 a1 a2 a3
+phi(x)=a0*x^3+a1*x^2+a2*x+a3
+eq1=int((phi(x)-f(x))*x^3,x,a,b)
+eq2=int((phi(x)-f(x))*x^2,x,a,b)
+eq3=int((phi(x)-f(x))*x,x,a,b)
+eq4=int((phi(x)-f(x)),x,a,b)
+[a01 a11 a21 a31]=solve([eq1 eq2 eq3 eq4],[a0 a1 a2 a3])
+a01=vpa(a01)
+a11=vpa(a11)
+a21=vpa(a21)
+a31=vpa(a31)
+phi(x)=subs(phi(x),[a0 a1 a2 a3],[a01 a11 a21 a31])
+% - 0.441*x^3 - 0.173*x^2 + 1.61*x - 0.00226
+
+X=0:0.1:1
+Y1=vpa(f(X))
+% [ 0, 0.156, 0.309, 0.454, 0.588, 0.707, 0.809, 0.891, 0.951, 0.988, 1.0]
+Y2=phi(X)
+% [ -0.00226, 0.157, 0.31, 0.454, 0.587, 0.706, 0.808, 0.891, 0.952, 0.988, 0.997]
+D=Y1-Y2
+% [ 0.00226, -4.87e-4, -9.86e-4, -3.45e-4, 5.12e-4, 9.39e-4, 6.47e-4, -2.27e-4, -0.00105, -6.61e-4, 0.00269]
+
+plot(X,Y1)
+hold on
+plot(X,Y2)
+grid on
+hold off
---
+++ 2.20.8_The_simplest_ways_to_process_experimental data/ex1263.m
@@ -0,0 +1,33 @@
+clear
+digits(4)
+syms x
+f(x)=log(4+x)
+a=0
+b=1
+syms a0 a1 a2 a3
+phi(x)=a0*x^3+a1*x^2+a2*x+a3
+eq1=int((phi(x)-f(x))*x^3,x,a,b)
+eq2=int((phi(x)-f(x))*x^2,x,a,b)
+eq3=int((phi(x)-f(x))*x,x,a,b)
+eq4=int((phi(x)-f(x)),x,a,b)
+[a01 a11 a21 a31]=solve([eq1 eq2 eq3 eq4],[a0 a1 a2 a3])
+a01=vpa(a01)
+a11=vpa(a11)
+a21=vpa(a21)
+a31=vpa(a31)
+phi(x)=subs(phi(x),[a0 a1 a2 a3],[a01 a11 a21 a31])
+% 0.003689*x^3 - 0.03036*x^2 + 0.2498*x + 1.386
+
+X=0:0.1:1
+Y1=vpa(f(X))
+% [ 1.386, 1.411, 1.435, 1.459, 1.482, 1.504, 1.526, 1.548, 1.569, 1.589, 1.609]
+Y2=phi(X)
+% [ 1.386, 1.411, 1.435, 1.459, 1.482, 1.504, 1.526, 1.548, 1.569, 1.589, 1.609]
+D=Y1-Y2
+% [ -9.28e-6, 2.177e-6, 3.548e-6, 7.09e-7, -2.399e-6, -3.592e-6, -2.289e-6, 6.243e-7, 2.92e-6, 1.14e-6, -9.306e-6]
+
+fplot(f(x),[a b])
+hold on
+fplot(phi(x),[a b])
+grid on
+hold off
