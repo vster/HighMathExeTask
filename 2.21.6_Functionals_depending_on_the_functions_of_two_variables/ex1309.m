f951b52e95877aa08dbfb8625c82ba42029e69de
Vital Teresh <vster2006@gmail.com>
2019-07-13 15:06:55 +0300

    modify ex1309-1310

--- 2.21.6_Functionals_depending_on_the_functions_of_two_variables/ex1309.m
+++ 2.21.6_Functionals_depending_on_the_functions_of_two_variables/ex1309.m
@@ -17,11 +17,12 @@ dFdp=subs(dFdp,[p q],[p1 q1])
 dFdq=subs(dFdq,[p q],[p1 q1])
 % q1(x, y)/(p1(x, y)^2 + q1(x, y)^2 + 1)^(1/2)
 
-eq1=simplify(diff(dFdp,x)+diff(dFdq,y))
+eq1=simplify(dFdz-diff(dFdp,x)-diff(dFdq,y))
 
 syms r s t
 % d2z/dx2=r,d2z/dxdy=s,d2z/dy2=t
 eq2=subs(eq1,[diff(p1(x, y), x) diff(q1(x, y), x)... 
               diff(p1(x, y), y) diff(q1(x, y), y)],[r s s t])
-eq3=subs(eq2,[p1 q1],[p q])  
+eq3=subs(eq2,[p1 q1],[p q]) 
+eq3=-eq3
 % (t*p^2 - 2*s*p*q + r*q^2 + r + t)/(p^2 + q^2 + 1)^(3/2)
