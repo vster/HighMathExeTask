a1029abb0d9ad428c58f8895719a52573e4c6f3e
Vital Teresh <vster2006@gmail.com>
2019-08-08 23:41:21 +0300

    add ex83_87_91

---
+++ 1.1.2_Line/divseg2.m
@@ -0,0 +1,3 @@
+function C=divseg2(A,B,lambda)
+C=[(A(1)+lambda*B(1))/(1+lambda) (A(2)+lambda*B(2))/(1+lambda)];
+end
---
+++ 1.1.2_Line/seglen.m
@@ -0,0 +1,3 @@
+function d=seglen(A,B)
+d=sqrt((B(1)-A(1))^2+(B(2)-A(2))^2);
+end
