f2774bae67322fae3b1121b42c5a30edca7e0f86
Vital Teresh <vster2006@gmail.com>
2019-09-10 13:47:02 +0300

    add ex1610_1615

--- 1.10.4_Calculating_the_arc_length_of_a_plane_curve/ex1611.m
+++ 1.10.4_Calculating_the_arc_length_of_a_plane_curve/ex1611.m
@@ -2,21 +2,16 @@
 % x=cos(t)^5,y=sin(t)^5 from x1=0 to t2=pi/2
 
 % L = int (t1-t2) sqrt(diff(x)^2+diff(y)^2) dt
-syms t;
+syms t real;
 x=cos(t)^5
 y=sin(t)^5
 
 dx=diff(x,t)
 dy=diff(y,t)
 
-c1=dx^2+dy^2
-c1=factor(c1)
-pretty(c1)
+c=simplify(sqrt(dx^2+dy^2))
 
-c=simplify(sqrt(c1))
-pretty(c)
-
-L=int(c,0,pi/2)
+L=simplify(int(c,0,pi/2))
 % Does not work
 
 % Correct answer
